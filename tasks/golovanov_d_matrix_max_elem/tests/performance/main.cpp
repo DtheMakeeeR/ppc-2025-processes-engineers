6235a9c468ed297cd0381261a13d55ba53ed9720
DtheMakeeeR <danilgol45@mail.ru>
2025-11-19 17:58:09 +0300

    tests

--- tasks/golovanov_d_matrix_max_elem/tests/performance/main.cpp
+++ tasks/golovanov_d_matrix_max_elem/tests/performance/main.cpp
@@ -15,7 +15,7 @@ class GolovanovDMatrixMaxElemPerfTest : public ppc::util::BaseRunPerfTests<InTyp
  public:
   void SetUp() override {
     std::vector<double> tmpVector(0);
-    int n = 1000, m = 1000;
+    int n = 2000, m = 2000;
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
         double value = i + j;
@@ -30,6 +30,7 @@ class GolovanovDMatrixMaxElemPerfTest : public ppc::util::BaseRunPerfTests<InTyp
     if (output_data == maximum) {
       return true;
     }
+    std::cout << "output data " << output_data;
     return false;
   }
 
