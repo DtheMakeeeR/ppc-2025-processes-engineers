7e4a8d08cda7eb005cdc8637ffd9543c68dc5d0e
DtheMakeeeR <danilgol45@mail.ru>
2025-12-20 15:21:18 +0300

    static func

--- tasks/golovanov_d_bcast/mpi/src/ops_mpi.cpp
+++ tasks/golovanov_d_bcast/mpi/src/ops_mpi.cpp
@@ -9,7 +9,7 @@
 #include "golovanov_d_bcast/common/include/common.hpp"
 
 namespace golovanov_d_bcast {
-int MyBcast(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm) {
+static int MyBcast(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm) {
   int real_rank = 0;
   MPI_Comm_rank(comm, &real_rank);
   int world_size = 0;
